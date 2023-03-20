a0712f83cd2c3935ae0d1ae03187403661840739
Gihyeon Sung <0@gihyeon.xyz>
2023-03-20 11:32:16 +0900

    Set nvom lsp config

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -41,5 +41,28 @@ let g:signify_sign_delete_first_line = '-'
 let g:signify_sign_show_count = 0
 ]])
 
-local nvim_lsp = require('lspconfig')
-nvim_lsp.tsserver.setup {}
+local lsp = require('lspconfig')
+lsp.lua_ls.setup {}
+lsp.tsserver.setup {}
+lsp.pyright.setup {}
+
+vim.api.nvim_create_autocmd('LspAttach', {
+  group = vim.api.nvim_create_augroup('UserLspConfig', {}),
+  callback = function(ev)
+    -- Enable completion triggered by <c-x><c-o>
+    vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
+
+    -- Buffer local mappings.
+    -- See `:help vim.lsp.*` for documentation on any of the below functions
+    local opts = { buffer = ev.buf }
+    vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+    vim.keymap.set('n', '<leader>gd', vim.lsp.buf.definition, opts)
+    vim.keymap.set('n', '<leader>gr', vim.lsp.buf.references, opts)
+    vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, opts)
+    vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, opts)
+    vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
+    vim.keymap.set('n', '<leader>fm', function()
+      vim.lsp.buf.format { async = true }
+    end, opts)
+  end,
+})
