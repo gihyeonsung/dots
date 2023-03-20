431edac0ac8c6e0f9e0cf20089f9551fb778e0e6
Gihyeon Sung <0@gihyeon.xyz>
2023-03-20 11:11:57 +0900

    Use init.lua

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,21 +1,14 @@
-" VIM-PLUG PLUGINS
+vim.cmd([[
 call plug#begin()
-
-" file browser
 Plug 'scrooloose/nerdtree'
-
-" fuzzy finder
 Plug 'junegunn/fzf.vim'
 Plug 'junegunn/fzf', { 'do': { -> fzf#install() } }
-
-" color
 Plug 'morhetz/gruvbox'
 Plug 'mhinz/vim-signify'
 Plug 'sheerun/vim-polyglot'
-
+Plug 'neovim/nvim-lspconfig'
 call plug#end()
 
-" NEOVIM SETTINGS
 colorscheme gruvbox
 set colorcolumn=80
 set expandtab
@@ -32,47 +25,21 @@ set smartcase
 set tabstop=2
 set termguicolors
 
-augroup fsharp
-  autocmd FileType fsharp setlocal tabstop=2
-  "autocmd FileType fsharp setlocal tabstop=4 " for pet projects
-augroup end
-
-augroup go
-  autocmd FileType go setlocal noexpandtab
-  autocmd FileType go setlocal tabstop=4
-augroup end
-
-augroup sh
-  autocmd FileType sh,zsh setlocal noexpandtab
-  autocmd FileType sh,zsh setlocal tabstop=8
-augroup end
-
-augroup c
-  autocmd FileType c setlocal noexpandtab
-  autocmd FileType c setlocal tabstop=8
-  autocmd BufEnter *.h setlocal filetype=c
-augroup end
-
-augroup arduino
-  autocmd FileType arduino setlocal noexpandtab
-  autocmd FileType arduino setlocal tabstop=8
-augroup end
-
-" NERDTREE
 let NERDTreeBookmarksFile = stdpath("data") . "NERDTreeBookmarks"
 let NERDTreeDirArrowCollapsible = ""
 let NERDTreeDirArrowExpandable = ""
 let NERDTreeShowHidden = 1
 nnoremap <silent> <leader>e :NERDTreeToggle<cr>
 
-" FZF.VIM
 nnoremap <leader>ff :Files<cr>
 nnoremap <leader>fr :Rg<cr>
 
-" VIM-SIGNIFY
 let g:signify_sign_add = '+'
 let g:signify_sign_change = '*'
 let g:signify_sign_delete = '-'
 let g:signify_sign_delete_first_line = '-'
 let g:signify_sign_show_count = 0
+]])
 
+local nvim_lsp = require('lspconfig')
+nvim_lsp.tsserver.setup {}
